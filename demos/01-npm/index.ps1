a5c40a3499ff3d0f85ea3d57d9dcf2bd0e743447
Adrien Clerbois <adrien.clerbois@outlook.com>
2023-05-25 20:16:19 +0200

    Init project and add demo npm

---
+++ demos/01-npm/index.ps1
@@ -0,0 +1,19 @@
+# Init project playwright demo 
+
+npm init playwright@latest
+
+# Show basic test
+
+# Explain locator
+
+# Run test
+
+npx playwright test
+
+# Display result
+
+npx playwright show-report
+
+# Move file from tests-examples to tests and run command
+
+npx playwright test --ui
